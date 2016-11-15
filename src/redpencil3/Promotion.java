d60d2943578b6e97dd098caaab87109fc79c0f38
tony1960 <anthony.sterrett@gmail.com>
2016-11-15 10:08:04 -0500

    furtherPriceReduction. Added code to test furtherPriceReduction

--- src/redpencil3/Promotion.java
+++ src/redpencil3/Promotion.java
@@ -76,5 +76,38 @@ public class Promotion {
         }
         return false;
     }
+    
+        /**
+     * Further reduces a reduced price promotion. if reduction is out of range
+     * it with sets reducedPrice and StableDays to zero
+     *
+     * @param reduction the price to reduce reducedPrice too.
+     */
+    public void furtherPriceReduction(Float reduction) {
+
+        Float low;
+        Float high;
+
+        low = price * .70F;
+
+        System.out.println(" compare reduction " + reduction);
+
+        if (reduction.compareTo(low) < 0) {
+
+            System.out.println(" compare low = " + reducedPrice.compareTo(low));
+            System.out.println("reducedPrice = " + reducedPrice);
+
+            stableDays = 0;
+            reducedPrice = 0.0F;
+
+            System.out.println("low = " + low + " " + "reduction " + reduction);
+            return;
+        }
+
+        reducedPrice = reduction;
+
+    }
+
+    
 
 }
