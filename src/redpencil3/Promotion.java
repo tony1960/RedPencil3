1b385044fb9e346308aa90e6bc75602cad85e4bb
tony1960 <anthony.sterrett@gmail.com>
2016-11-15 09:22:42 -0500

    Added priceReduction code

--- src/redpencil3/Promotion.java
+++ src/redpencil3/Promotion.java
@@ -10,21 +10,22 @@ package redpencil3;
  * @author tonys
  */
 public class Promotion {
+
     /**
      * InitialStableDays. Sets the number of days at a Stable price.
      *
      * @param stableDays Stable days.
      */
-    
-     Float price = 0.0F;
+
+    Float price = 0.0F;
     Float reducedPrice = 0.0F;
     int stableDays = 0;
-    
+
     public void InitialStableDays(int stableDays) {
         this.stableDays = stableDays;
     }
 
-      /**
+    /**
      * InitialPrice. Sets the Price for start of test.
      *
      * @param price Price value.
@@ -32,5 +33,36 @@ public class Promotion {
     public void InitialPrice(Float price) {
         this.price = price;
     }
-    
+
+    /**
+     * priceRedution . Reduces price to reducedPrice price.
+     *
+     * @param reducedPrice Price value.
+     * @return true - if reduced price is with range otherwise false
+     */
+    public Boolean priceReduction(Float reducedPrice) {
+
+        if (stableDays < 30) {
+            return false;
+        }
+
+        Float low;
+        Float high;
+
+        low = price * .70F;
+        high = price * .95F;
+
+        if (reducedPrice.compareTo(low) < 0) {
+            return false;
+        }
+        if (reducedPrice.compareTo(high) > 0) {
+            return false;
+        }
+
+        this.stableDays = 0;
+        this.reducedPrice = reducedPrice;
+
+        return true;
+    }
+
 }
