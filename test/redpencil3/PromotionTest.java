a6b06ab2131f5feabc1a4919c2e44d887acf1876
tony1960 <anthony.sterrett@gmail.com>
2016-11-15 09:05:02 -0500

    First Commit  Test

---
+++ test/redpencil3/PromotionTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redpencil3;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tonys
+ */
+public class PromotionTest {
+    
+    public PromotionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testInitialStableDays() {
+        System.out.println("testInitialStableDays");
+        Promotion instance = new Promotion();
+        int days = 0;
+        int expectedResult = 29;
+        instance.InitialStableDays(29);
+        int result = instance.stableDays;
+
+        assertEquals(expectedResult, result);
+    }
+    
+}
